a7ae7389328a58cc1373c6299c7ba70c0f507648
Panda Ma <1666088491@qq.com>
2023-03-23 22:40:12 +0800

    test2

--- router/my.js
+++ router/my.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/getUser', async (req, res) => {
+    const test=req
     res.send({
         code: 200,
         data: {
-            req:1
+            req:test
         },
     })
 })
