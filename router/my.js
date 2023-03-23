e59d31404e551f48cde4f066fd69ea07a93bb013
Panda Ma <1666088491@qq.com>
2023-03-23 22:36:49 +0800

    test1

--- router/my.js
+++ router/my.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const {List, Good} = require("../db");
 const router = express.Router()
 
 router.get('/getUser', async (req, res) => {
     res.send({
         code: 200,
         data: {
-            req:req
+            req:1
         },
     })
 })
